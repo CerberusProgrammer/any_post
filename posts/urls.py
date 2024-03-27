95a7b64e5b96b15a759aafd371dfe8a18c120924
Omar Flores <cerberusprogrammer@gmail.com>
2024-03-27 08:33:19 -0700

    New templates for the posts

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.posts, name='posts'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
-def dashboard(request):
-    return render(request, 'dashboard.html')
+def posts(request):
+    posts = Post.objects.all().order_by('-id')[:10]
+    return render(request, 'posts.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post.html', {'post': post})
