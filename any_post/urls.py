bed33e425a2d9c9eada7ca4a9b94f8f018329392
Omar Flores <cerberusprogrammer@gmail.com>
2024-03-27 08:33:33 -0700

    Little updates

--- any_post/urls.py
+++ any_post/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('posts/', include('posts.urls'))
 ]
