52cce43f48fc620ed162417dfe81d6ef8b2fce64
Omar Flores <cerberusprogrammer@gmail.com>
2024-03-26 20:41:36 -0700

    New project!

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './posts/templates/posts/**/*.html', // Agrega las rutas a otras aplicaciones si es necesario
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
